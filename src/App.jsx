51b0d2230cfa5147a90dbbbf2a41ee54d60c617a
ToniJ-22 <rachelrene1524@gmail.com>
2026-02-27 18:58:26 -0500

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react';
-import Home from '../pages/home';
-import Favorites from '../pages/favorites';
-import Navbar from '../components/navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Favorites from './pages/favorites';
+import Navbar from './components/navbar';
 import "../src/styles.css"
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
-    </BrowserRouter>
+      </div>
   );
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
+import { store } from './redux/store';
 import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import {StrictMode} from 'react';
+ReactDOM.render(
+  <StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
 
